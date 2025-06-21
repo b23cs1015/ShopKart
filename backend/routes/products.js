a4997214a3db4c19f3902d1ecd0de53414075be4
Devesh Labana <b23cs1015@iitj.ac.in>
2025-06-21 23:28:03 +0530

    added glow effect

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const Product = require('../models/Product');
 
 // Get all products
-router.get('/', async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-});
+ router.get('/', async (req, res) => {
+   const products = await Product.find();
+   res.json(products);
+ });
+
+
 
 // Add a new product
 router.post('/', async (req, res) => {
