0e5f99cbe306aae19294c4d7dace4654ba1b883e
Devesh Labana <b23cs1015@iitj.ac.in>
2025-06-22 14:50:12 +0530

    added 3 cards on home page

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './HomePage.module.css';
-import ProductList from '../ProductList';
+
+const categories = [
+  {
+    name: 'Fashion',
+    image: 'https://source.unsplash.com/600x400/?fashion,clothes',
+    subcategories: ['Men', 'Women', 'Kids']
+  },
+  {
+    name: 'Electronics',
+    image: 'https://source.unsplash.com/600x400/?electronics,gadgets',
+    subcategories: ['Mobiles', 'Laptops', 'Accessories']
+  },
+  {
+    name: 'Home',
+    image: 'https://source.unsplash.com/600x400/?furniture,home',
+    subcategories: ['Furniture', 'Kitchen', 'Decor']
+  }
+];
 
 const HomePage = () => {
   return (
-    <></>
+    <div className={styles.cardGrid}>
+    {categories.map(cat => (
+        <div key={cat.id} className={styles.card}>
+        <div className={styles.name}>{cat.name}</div>
+        <img src={cat.image} alt={cat.name} className={styles.image} />
+        <div className={styles.subcategories}>
+            {cat.subcategories.map(sub => (
+            <div key={sub} className={styles.sub}>{sub}</div>
+            ))}
+        </div>
+        </div>
+    ))}
+    </div>
+
+
   );
 };
 
